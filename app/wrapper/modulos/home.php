85707ea40f1517a443155c643fadd9c6599f7dcb
Erick Itzama Vasquez Ruiz <erick.vasquez@controlintegral.mx>
2019-05-16 22:34:10 -0500

    Se pulen los datos del home

---
+++ app/wrapper/modulos/home.php
@@ -0,0 +1,244 @@
+<div id="header">
+	<div class="flexslider">
+		<ul class="slides">
+			<li class="slider-item" style="background-image: url('<?php echo URL; ?>/slider/item1/')">
+				<div class="intro container">
+					<div class="inner-intro">
+						<h1 class="header-title">
+							<span>Viajar</span> siempre "Que buena idea"
+						</h1>
+						<p class="header-sub-title">
+							una experiencia que te deja sin palabras.
+						</p>
+						<button class="btn custom-btn">
+							Ver ahora
+						</button>
+					</div>
+				</div>
+			</li>
+			<li class="slider-item" style="background-image: url('<?php echo URL; ?>/slider/item2/')">
+				<div class="intro">
+					<div class="inner-intro">
+						<h1 class="header-title">
+							<span>Viajar</span> es una manera de <span>vivir</span>
+						</h1>
+					</div>
+				</div>
+			</li>
+		</ul>
+	</div>
+</div>
+<section class="tour section-wrapper container">
+	<h2 class="section-title">Encontrar un viaje por sitios de interes</h2>
+	<p class="section-subtitle">
+		¿A dónde quieres ir?
+	</p>
+	<div class="row">
+		<div class="col-md-3 col-sm-6">
+			<form role="form" class="form-dropdown">
+				<div class="form-group">
+					<label for="sel1">Selecciona un lugar de la lista:</label>
+					<select class="form-control border-radius" id="sel1">
+						<option>Mazunte</option>
+						<option>Puerto Ángel</option>
+						<option>Zipolite</option>
+					</select>
+				</div>
+			</form>
+		</div>
+		<div class="col-md-3 col-sm-6">
+			<div class="input-group">
+				<input type="text" class="form-control border-radius border-right" placeholder="Día de llegada"/>
+				<span class="input-group-addon border-radius custom-addon">
+					<i class="ion-ios-calendar"></i>
+				</span>
+			</div>
+		</div>
+		<div class="col-md-3 col-sm-6">
+			<div class="input-group">
+				<input type="text" class="form-control border-radius border-right" placeholder="Día de partida"/>
+				<span class="input-group-addon border-radius custom-addon">
+					<i class="ion-ios-calendar"></i>
+				</span>
+			</div>
+		</div>
+		<div class="col-md-3 col-sm-6">
+			<div class="btn btn-default border-radius custom-button">
+				Buscar
+			</div>
+		</div>
+	</div>
+</section>
+<section class="offer section-wrapper">
+	<div class="container">
+		<h2 class="section-title">Nuestras ofertas</h2>
+		<p class="section-subtitle">
+			Aprovecha estas ofertas de temporada baja.
+		</p>
+		<div class="row">
+			<div class="col-sm-3 col-xs-6">
+				<div class="offer-item">
+					<div class="icon">
+						<i class="ion-social-euro"></i>
+					</div>
+					<h3>Precios económicos.</h3>
+					<p>
+						Encuentra los mejores precios de "temporada baja".
+					</p>
+				</div>
+			</div>
+			<div class="col-sm-3 col-xs-6">
+				<div class="offer-item">
+					<div class="icon">
+						<i class="ion-ios-home"></i>
+					</div>
+					<h3>Comodidad.</h3>
+					<p>
+						Hoteles, posadas y camping desde tan solo 20 mtrs. (Aprox.) de la playa.
+					</p>
+				</div>
+			</div>
+			<div class="col-sm-3 col-xs-6">
+				<div class="offer-item">
+					<div class="icon">
+						<i class="ion-android-bus"></i>
+					</div>
+					<h3>Transporte.</h3>
+					<p>
+						Transporte seguro a solo metros de su habitación.
+					</p>
+				</div>
+			</div>
+			<div class="col-sm-3 col-xs-6">
+				<div class="offer-item">
+					<div class="icon">
+						<i class="ion-ios-locked"></i>
+					</div>
+					<h3>Seguridad.</h3>
+					<p>
+						Habitaciones seguras y confiables.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="visit section-wrapper">
+	<div class="container">
+		<h2 class="section-title">Lugares a visitar.</h2>
+		<p class="section-subtitle">
+			Visita los lugares aledañas a su hotel.
+		</p>
+		<div class="owl-carousel visit-carousel" id="">
+			<div class="item">
+				<img src="<?php echo URL; ?>/visitplace/item1/" alt="visit-image" class="img-responsive visit-item">
+			</div>
+			<div class="item">
+				<img src="<?php echo URL; ?>/visitplace/item2/" alt="visit-image" class="img-responsive visit-item">
+			</div>
+			<div class="item">
+				<img src="<?php echo URL; ?>/visitplace/item3/" alt="visit-image" class="img-responsive visit-item">
+			</div>
+			<div class="item">
+				<img src="<?php echo URL; ?>/visitplace/item1/" alt="visit-image" class="img-responsive visit-item">
+			</div>
+			<div class="item">
+				<img src="<?php echo URL; ?>/visitplace/item2/" alt="visit-image" class="img-responsive visit-item">
+			</div>
+			<div class="item">
+				<img src="<?php echo URL; ?>/visitplace/item3/" alt="visit-image" class="img-responsive visit-item">
+			</div>
+		</div>
+	</div>
+</section>
+<div class="offer-cta">
+	<div class="container">
+		<div class="offering">
+			<div class="percent">
+				<span>15%</span> de descuento
+			</div>
+			<div class="FTour">
+				en <strong>Viaje Familiar</strong>
+			</div>
+			<a class="btn btn-default price-btn" href="#">
+				Consulta nuestros precios
+			</a>
+		</div>
+	</div>
+</div>
+<section class="additional-services section-wrapper">
+	<div class="container">
+		<h2 class="section-title">Atractivos adicionales</h2>
+		<p class="section-subtitle"></p>
+		<div class="row">
+			<div class="col-md-4 col-sm-6">
+				<div class="custom-table">
+					<img src="<?php echo URL; ?>//atractives/item1/" alt="" class="add-srvc-img">
+					<div class="add-srvc-detail">
+						<h4 class="add-srvc-heading">Fotografia</h4>
+						<p class="add-srvc">
+							Disfruta de los mejores amaneceres y puestas de sol.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-6">
+				<div class="custom-table">
+					<img src="<?php echo URL; ?>/atractives/item2/" alt="" class="add-srvc-img">
+					<div class="add-srvc-detail">
+						<h4 class="add-srvc-heading">Ciclismo</h4>
+						<p class="add-srvc">
+							Disfruta de los mejores paisajes haciendo un buen ejercicio .
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-6">
+				<div class="custom-table">
+					<img src="<?php echo URL; ?>/atractives/item3/" alt="" class="add-srvc-img">
+					<div class="add-srvc-detail">
+						<h4 class="add-srvc-heading">Senderismo</h4>
+						<p class="add-srvc">
+							Las mejores rutas de caminata con flora y fauna espectacular.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<div class="section-wrapper sponsor">
+	<div class="container">
+		<div class="owl-carousel sponsor-carousel">
+			<div class="item">
+				<a href="#">
+					<img src="<?php echo URL; ?>/images/itzamaposada.png" alt="sponsor-brand" class="img-responsive sponsor-item">
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="subscribe section-wrapper">
+	<a class="brand-logo" href="index.html" title="HOME"><i class="ion-paper-airplane"></i> Oaxaca <span>Viajes Con Experiencia</span></a>
+	<p class="subscribe-now">
+		Suscribete a nuestras noticias
+	</p>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 col-sm-6 col-md-offset-4 col-sm-offset-3">
+				<div class="input-group">
+					<input type="email" class="form-control border-radius" placeholder="Correo Electrónico">
+					<span class="input-group-btn">
+						<button class="btn btn-default border-radius custom-sub-btn" type="button">Aceptar</button>
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>	
+	<ul class="social-icon">
+		<li><a href="#"><i class="ion-social-twitter"></i></a></li>
+		<li><a href="#"><i class="ion-social-facebook"></i></a></li>
+		<li><a href="#"><i class="ion-social-linkedin-outline"></i></a></li>
+		<li><a href="#"><i class="ion-social-googleplus"></i></a></li>
+	</ul>
+</div>
